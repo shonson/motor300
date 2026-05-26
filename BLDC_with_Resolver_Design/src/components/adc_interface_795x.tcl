618cbf31085ba3ed69f915ef565e304777b231e6
shonson <43637768+shonson@users.noreply.github.com>
2026-05-26 11:53:27 +0000

    add BLDC_with_Resolver_Design

---
+++ BLDC_with_Resolver_Design/src/components/adc_interface_795x.tcl
@@ -0,0 +1,4 @@
+# Exporting core adc_interface_795x to TCL
+# Exporting Create HDL core command for module adc_interface_795x
+create_hdl_core -file {hdl/adc_interface_795x.vhd} -module {adc_interface_795x} -library {work} -package {}
+# Exporting BIF information of  HDL core command for module adc_interface_795x
